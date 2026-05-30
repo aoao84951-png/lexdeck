224b6a7e26a6431922be7ea0224f188cd8765811
aoao84951-png <aoao84951@gmail.com>
2026-05-30 17:52:55 +0900

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 import MobileApp from "@/components/MobileApp";
+import DesktopApp from "@/components/DesktopApp";
 
 export default function Home() {
-  return <MobileApp />;
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useEffect(() => {
+    const checkScreen = () => {
+      setIsDesktop(window.innerWidth >= 1024);
+    };
+
+    checkScreen();
+
+    window.addEventListener("resize", checkScreen);
+
+    return () => {
+      window.removeEventListener("resize", checkScreen);
+    };
+  }, []);
+
+  return isDesktop ? <DesktopApp /> : <MobileApp />;
 }
